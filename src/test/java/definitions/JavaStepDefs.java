b1a65ab9de225ea4aaac9ce2a85985592d5bd8c8
SergeyK <98507907+Sergey-828@users.noreply.github.com>
2022-06-30 19:14:23 -0700

    JavaDay4_3

--- src/test/java/definitions/JavaStepDefs.java
+++ src/test/java/definitions/JavaStepDefs.java
@@ -1,6 +1,7 @@
 package definitions;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
 public class JavaStepDefs {
@@ -13,5 +14,34 @@ public class JavaStepDefs {
     @And("I say {string}")
     public void iSay(String word) {
         System.out.println(word);
+    }
+
+
+    @Given("I perform actions with {string}")
+    public void iPerformActionsWith(String word) {
+        System.out.println(word.length());
+        System.out.println(word.getClass());
+        System.out.println(word.toLowerCase());
+        System.out.println(word.toUpperCase());
+        System.out.println(word.trim());
+        System.out.println(word.isEmpty());
+
+
+    }
+
+    @Given("I perform actions with {string} and {string}")
+    public void iPerformActionsWithAnd(String var1, String var2) {
+        System.out.println(var1 + var2);
+        System.out.println(var1.toUpperCase() + var2.toUpperCase());
+        System.out.println(var1.length() + var2.length());
+        System.out.println((var1.equals(var2)));
+        System.out.println(var1.equalsIgnoreCase(var2));
+        System.out.println(var1.contains(var2));
+
+
+
+
+
+
     }
 }
--- src/test/resources/features/Java.feature
+++ src/test/resources/features/Java.feature
@@ -6,3 +6,15 @@
       When I hello world
       And I say "Hello"
       And I say "Heya"
+
+    Scenario: Methods Exercises Name
+        Given I perform actions with "Name"
+
+
+    Scenario: Methods Exercises var and VAR
+      Given I perform actions with "my var" and "my VAR"
+
+
+
+
+
